6c67317a264f8b3d40fe1874ae37127e091221ea
nick bronn <ntbronn@us.ibm.com>
2023-02-19 20:14:46 -0500

    added some layering methods to backend.py

--- qiskit_research/utils/backend.py
+++ qiskit_research/utils/backend.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Utilities for dealing with backends."""
+"""Utilities for dealing with backends and their coupling maps."""
 
 
 from typing import Optional
@@ -31,3 +31,54 @@ def get_backend(
     if name == "statevector_simulator":
         return BasicAer.get_backend("statevector_simulator")
     raise ValueError("The given name does not match any supported backends.")
+
+def get_coupling_map_from_init_layout(coupling_map, init_layout):
+    """
+    TODO: write this function
+    """
+
+def get_outward_coupling_map(coupling_map, ent_map, start_qubits):
+    """
+    Recursive method for rearranging a coupling map to generate an entanglement
+    map outward from a set of starting qubit(s). The new entanglement map is then
+    suitable for use by canned algorithms such as TwoLocal in the entanglement= kwarg.
+
+    Args:
+        coupling_map: coupling map of backend (TODO: need to currently deepcopy this before calling method)
+        ent_map: rearranged coupling map used for generating entanglement in correct order (TODO: currently take empty list)
+        start_qubits: qubits to find the pairs containing (TODO: currently takes [middle_qubit])
+    """
+    next_qubits = []
+    for pair in [pair for pair in coupling_map if pair[0] in start_qubits]:
+        next_qubits.append(pair[1])
+        ent_map.append(pair)
+        coupling_map.remove(pair)
+        coupling_map.remove(list(reversed(pair)))
+
+    if next_qubits:
+        get_outward_coupling_map(coupling_map, ent_map, next_qubits)
+
+
+def get_layered_ansatz_coupling_map(coupling_map):
+    """
+    Method to layer entangling gates of an ansatz type into those that can be executed
+    simultaneously. Currently assumes spacing of 1 qubit within a layer.
+    TODO: extend to more than single-qubit spacing
+    """
+    # TODO: may want to make the below directional to support certain backends, i.e. ibm_sherbrooke
+    ordered_cm = [pair for pair in coupling_map if pair[0] < pair[1]]
+
+    # split entanglement map into sets of non-overlapping qubits
+    ent_map = []
+    for pair in ordered_cm:
+        if ent_map == []:
+            ent_map.append([pair])
+        elif all([any([pair[0] in epair or pair[1] in epair for epair in emap]) for emap in ent_map]):
+            ent_map.append([pair])
+        else:
+            for emap in ent_map:
+                if all([pair[0] not in epair and pair[1] not in epair for epair in emap]):
+                    emap.append(pair)
+                    break
+
+    return ent_map
